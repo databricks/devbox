98b637e5f21b5ba8fb78bada220ec23570f347ab
Li Haoyi <haoyi.li@databricks.com>
2019-10-31 10:08:44 +0800

    Move `computeSignatures` to `SigActor`

--- devbox/src/SigActor.scala
+++ devbox/src/SigActor.scala
@@ -1,13 +1,10 @@
 package devbox
-import devbox.common.{Sig, SyncLogger, ActorContext, Util, StateMachineActor}
-import scala.concurrent.Future
-object SigActor{
-  sealed trait Msg
-  case class SinglePath(scanRoot: os.Path, sub: os.SubPath) extends Msg
-  case class ManyPaths(group: Map[os.Path, Set[os.SubPath]]) extends Msg
-  case class LocalScanComplete() extends Msg
-  case class ComputeComplete() extends Msg
-}
+import java.util.concurrent.LinkedBlockingQueue
+
+import devbox.common.{ActorContext, Sig, StateMachineActor, SyncLogger, Util}
+
+import scala.concurrent.{ExecutionContext, Future}
+
 class SigActor(sendToSyncActor: SyncActor.Msg => Unit,
                signatureTransformer: (os.SubPath, Sig) => Sig,
                logger: SyncLogger)
@@ -41,12 +38,71 @@ class SigActor(sendToSyncActor: SyncActor.Msg => Unit,
   def compute(groups: Map[os.Path, Set[os.SubPath]]) = {
     val computeFutures =
       for((k, vs) <- groups)
-      yield SyncFiles.computeSignatures(vs, k, signatureTransformer).map((k, _))
+      yield SigActor.computeSignatures(vs, k, signatureTransformer).map((k, _))
 
     Future.sequence(computeFutures).foreach{ results =>
       sendToSyncActor(SyncActor.Events(results.map{case (k, vs) => (k, vs.toMap)}.toMap))
       this.send(SigActor.ComputeComplete())
     }
     Busy(Map())
+  }
+}
+object SigActor{
+  sealed trait Msg
+  case class SinglePath(scanRoot: os.Path, sub: os.SubPath) extends Msg
+  case class ManyPaths(group: Map[os.Path, Set[os.SubPath]]) extends Msg
+  case class LocalScanComplete() extends Msg
+  case class ComputeComplete() extends Msg
+
+
+  def computeSignatures(eventPaths: Set[os.SubPath],
+                        src: os.Path,
+                        signatureTransformer: (os.SubPath, Sig) => Sig)
+                       (implicit ec: ExecutionContext)
+  : Future[Seq[(os.SubPath, Option[Sig])]] = {
+
+    val eventPathsLinks = eventPaths.map(p => (p, os.isLink(src / p)))
+    // Existing under a differently-cased name counts as not existing.
+    // The only way to reliably check for a mis-cased file on OS-X is
+    // to list the parent folder and compare listed names
+    val preListed = eventPathsLinks
+      .filter(_._2)
+      .map(_._1 / os.up)
+      .map(dir =>
+        (
+          dir,
+          if (!os.isDir(src / dir, followLinks = false)) Set[String]()
+          else os.list(src / dir).map(_.last).toSet
+        )
+      )
+      .toMap
+
+    val buffers = new LinkedBlockingQueue[Array[Byte]]()
+    for(i <- 0 until 6) buffers.add(new Array[Byte](Util.blockSize))
+
+    val futures = eventPathsLinks
+      .iterator
+      .map{ case (sub, isLink) =>
+        Future {
+
+          val abs = src / sub
+          val newSig = try {
+            if ((isLink && !preListed(sub / os.up).contains(sub.last)) ||
+              (!isLink && !os.followLink(abs).contains(abs))) None
+            else {
+              val attrs = os.stat(abs, followLinks = false)
+              val buffer = buffers.take()
+              try Sig
+                .compute(abs, buffer, attrs.fileType)
+                .map(signatureTransformer(sub, _))
+              finally buffers.put(buffer)
+            }
+          }catch{case e: Throwable => None}
+          (sub, newSig)
+        }
+      }
+
+    val sequenced = scala.concurrent.Future.sequence(futures.toSeq)
+    sequenced
   }
 }
